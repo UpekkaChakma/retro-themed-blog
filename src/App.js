286638f4ece87d8f4d97451c44765d7f098defb4
UpekkaChakma <upekkachakma@gmail.com>
2021-04-09 13:21:41 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Home from './Components/Home/Home';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+import { createContext, useState } from 'react';
+import Admin from './Components/Admin/Admin';
+import CheckOut from './Components/CheckOut/CheckOut';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NotFound from './Components/NotFound/NotFound';
+import Login from './Components/Login/Login';
+import ManageGames from './Components/ManageGames/ManageGames';
+import Orders from './Components/Orders/Orders';
+
+
+export const UserContext = createContext();
+
 
 function App() {
+  document.title = "The game shop";
+
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+
+          <PrivateRoute path="/checkout/:id">
+            <CheckOut></CheckOut>
+          </PrivateRoute>
+
+          <PrivateRoute path="/orderList">
+            <Orders></Orders>
+          </PrivateRoute>
+
+          <PrivateRoute path="/admin/addGame">
+            <Admin></Admin>
+          </PrivateRoute>
+
+          <PrivateRoute path="/admin/manageGames">
+            <ManageGames></ManageGames>
+          </PrivateRoute>
+
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB6R15C1s789V457jyIFC3NT96Po5V_pfg",
+    authDomain: "upex-city-travellers.firebaseapp.com",
+    projectId: "upex-city-travellers",
+    storageBucket: "upex-city-travellers.appspot.com",
+    messagingSenderId: "770397916402",
+    appId: "1:770397916402:web:c1cee58fff15e759be546b"
+  };
+export default firebaseConfig;
